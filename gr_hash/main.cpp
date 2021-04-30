20e82117a7f4428d02489352c9a60d45eb748a4b
aytacaksel <83459591+aytacaksel@users.noreply.github.com>
2021-04-30 16:57:13 +0300

    Add files via upload

---
+++ gr_hash/main.cpp
@@ -0,0 +1,37 @@
+#include "main.h"
+
+void DLL_EXPORT grinit()
+{
+    slow_hash_allocate_state();
+}
+
+void DLL_EXPORT grhash(const char* input, char* output)
+{
+    gr_hash(input, output);
+}
+
+
+
+extern "C" DLL_EXPORT BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+    switch (fdwReason)
+    {
+        case DLL_PROCESS_ATTACH:
+            // attach to process
+            // return FALSE to fail DLL load
+            break;
+
+        case DLL_PROCESS_DETACH:
+            // detach from process
+            break;
+
+        case DLL_THREAD_ATTACH:
+            // attach to thread
+            break;
+
+        case DLL_THREAD_DETACH:
+            // detach from thread
+            break;
+    }
+    return TRUE; // succesful
+}
